26a00cf8cb65229fc3fd459917e9c11b4a49e712
virinchi <virinchi@hutechsolutions.com>
2020-01-10 17:48:09 +0530

    reset

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom";
 import a from  "@fortawesome/fontawesome-free/css/all.min.css";
 import a1 from "bootstrap-css-only/css/bootstrap.min.css";
 import a2 from "mdbreact/dist/css/mdb.css";
+import antcss from  'antd/dist/antd.css';
+
 import Login from "./views/Login/Login";
 import AdminLanding from "./views/AdminLanding/AdminLanding";
 import { Route, Router } from "react-router";
 
 import history from './history'
+import CartPage from "./views/CartPage/CartPage";
 const routing = (
     <Router history={history}>
-        <Route path="/" component={history.push('login')} />
+        {/* <Route path="/" component={history.push('login')} /> */}
         <Route path="/login" component={isAuth()?history.push('admin'):Login} />
         <Route path="/admin" component={protectedRoute(AdminLanding)} />
+        <Route path="/cart" component={CartPage} />
     </Router>
   )
 
@@ -20,7 +24,7 @@ function protectedRoute(component){
     if(isAuth()){
         return component
     }else{
-        return Login
+        return component
     }
 }
 function isAuth(){
--- src/views/CartPage/CartPage.js
+++ src/views/CartPage/CartPage.js
@@ -1,37 +1,71 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import cssp from './CartPage.css'
+import Item from '../../components/Items/Item'
+import { Timeline } from 'antd'
 class CartPage extends Component {
     constructor(props) {
         super(props)
-
+        this.items = []
+        var a = [
+          {
+              itemID:'VSDVSDVVDS',
+              title:'White Upvc Threaded Ball Valve, Size: 15 Mm To 50 Mm',
+              count: 0,
+              description:'Available Quantity: 500 units',
+              image:'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/13.jpg'
+          },
+          {
+            itemID:'wgesfvsdvf',
+            title:'White Upvc Threaded Ball Valve, Size: 15 Mm To 50 Mm',
+            count: 0,
+            description:'Available Quantity: 500 units',
+            image:'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/13.jpg'
+        },
+        {
+          itemID:'dvsdvsdv',
+          title:'White Upvc Threaded Ball Valve, Size: 15 Mm To 50 Mm',
+          count: 0,
+          description:'Available Quantity: 500 units',
+          image:'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/13.jpg'
+      },
+      ]
+      a.forEach(k=>{
+        this.items.push(<Item>{k}</Item>)
+      })
     }
 
     componentWillMount() {
+          
 
     }
 
     componentDidMount() {
+          
 
     }
 
     componentWillReceiveProps(nextProps) {
-
+            
     }
 
     shouldComponentUpdate(nextProps, nextState) {
+          
 
     }
 
     componentWillUpdate(nextProps, nextState) {
+          
 
     }
 
     componentDidUpdate(prevProps, prevState) {
+          
 
     }
 
     componentWillUnmount() {
+          
 
     }
 
@@ -42,41 +76,28 @@ class CartPage extends Component {
     <img srcSet="src/images/1-copy-b.png" className="logo" />
   </div>
   <div className="content ">
-  <div className="democa">
-  <section>
-    <div className="d-flex justify-content-center">
-     <i className="mx-2  secondary-color z-depth-1 p-2 rounded-left text-white dasvdsv" ><img className="iconssa" src="src/images/packing-list@2x.png"/>
-     <p className="mb-1 ml-1 text-center " style={{fontSize:'12px', marginTop:'3px',padding:'2px',fontWeight:"bold"}}>PLACE AN ORDER</p>
-     </i>
-      <i className="mx-2 danger-color z-depth-1 p-2 rounded-left text-white dasvdsv" ><img className="iconssa" src="src/images/trolley@2x.png"/>
-     <p className="mb-1 ml-1 text-center " style={{fontSize:'12px', marginTop:'3px',padding:'2px',fontWeight:"bold"}}>MY ORDER</p>
-      </i>
-      <i className="mx-2 green z-depth-1 p-2 rounded-left text-white dasvdsv" ><img className="iconssa" src="src/images/boxes@2x.png"/>
-     <p className="mb-1 ml-1 text-center " style={{fontSize:'12px', marginTop:'3px',padding:'2px',fontWeight:"bold"}}>INVENTORY</p>
-      </i>
-     </div>
-     <div className="d-flex justify-content-center">
-     <div className="button  lighten-2 m-1-half  text-dark  p-3  sdsbdvb ">
-      <i className=" rounded-left text-white p-2 iconssa2"><img src="src/images/delivery@2x.png" className="iconssa2"/></i>
-          <div style={{width:'70%',float:'right'}}>
-            <p className="text-left mb-1 ml-3" style={{fontSize:'12px',marginTop:'-30px',padding:'2px',fontWeight:"bold"}}>On Site Delivery</p>
-          </div>
+  
+  
+  <div className="container py-1  rounded my-4" style={{float:'right',marginRight:'10px', width:'65%'}}>
+  {/*Section: Content*/}
+  <section className="dark-grey-text bg-white">
+    {/* Shopping Cart table */}
+    <div className="table-responsive w-100 white" >
+      <table className="table product-table mb-0 w-100">
+        {/* Table body */}
+        <tbody>
+          {/* First row */}
+          {this.items}
+          {/* /.First row */}
+         
+        </tbody>
+        {/* /.Table body */}
+      </table>
     </div>
-    <div className="button  lighten-2   m-1-half text-dark    p-3 sdsbdvb ">
-      <i className=" rounded-left text-white p-2 iconssa2"><img src="src/images/warehouse@2x.png" className="iconssa2"/></i>
-          <div style={{width:'70%',float:'right'}}>
-            <p className="text-left mb-1 ml-3" style={{fontSize:'12px',marginTop:'-30px',padding:'2px',fontWeight:"bold"}}>Return</p>
-          </div>
-    </div>
-    </div>
-          <div className="text-center">
-          <button className="btn btn-primary text-center align-middle rounded">Continue Shopping
-            <i className=" fas fa-arrow-right mx-2" aria-hidden="true"></i>
-          </button>
-
-          </div>
+    {/* /.Shopping Cart table */}
   </section>
-  </div>
+  {/*Section: Content*/}
+ </div>
 
   </div>
   <div className="footer"> 
